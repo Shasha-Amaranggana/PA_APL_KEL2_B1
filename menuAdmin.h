5aa158b26fe6682799457c4e62fdeb363b2004e1
Shasha-Amaranggana <dindashashaam@gmail.com>
2026-05-06 08:11:21 +0800

    mencoba

--- menuAdmin.h
+++
@@ -1,8 +0,0 @@
-#ifndef MENUADMIN_H
-#define MENUADMIN_H
-
-#include "data.h"
-
-int menuAdmin(Akun *akun, int indeksLogin, int &jumlahBuku);
-
-#endif
--- menuUser.h
+++
@@ -1,8 +0,0 @@
-#ifndef MENUUSER_H
-#define MENUUSER_H
-
-#include "data.h"
-
-int menuUser(Akun *akun, int indeksLogin, int &jumlahEbookUser);
-
-#endif
